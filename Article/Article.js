88eee392c5958900c8f99d21ec1041cb51646a90
Keenan <keenanhallas@gmail.com>
2020-04-16 15:46:16 -0400

    paragraph content added

--- Article/Article.js
+++ Article/Article.js
@@ -87,31 +87,64 @@ const data = [
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
   },
   {
-    title: `Breaking: Keenan is a bad web developer!`,
+    title: `Dialectic sublimation and subcultural materialist theory`,
     date: `Apr 14th, 2020`,
-    firstParagraph: `Honestly, have you seen how terrible this guy is?`,
+    firstParagraph: `Skateboard vape sustainable iPhone, farm-to-table raclette shabby chic vegan gluten-free readymade jianbing blog XOXO sartorial. 
+    Bespoke literally taxidermy, typewriter twee venmo celiac pop-up enamel pin lumbersexual microdosing. 90's godard forage shabby chic fixie artisan 
+    gluten-free keffiyeh. Poke snackwave woke, iPhone crucifix polaroid edison bulb. Pinterest heirloom fingerstache, keytar lumbersexual kogi 90's deep 
+    v mumblecore affogato sustainable chillwave franzen drinking vinegar cloud bread. Beard mumblecore kickstarter air plant, 8-bit raclette pinterest 
+    pabst activated charcoal. Church-key unicorn activated charcoal, pok pok heirloom fashion axe forage iceland skateboard bicycle rights kitsch marfa 
+    neutra whatever locavore.`,
 
-    secondParagraph: `Like, for real though.`,
+    secondParagraph: `Disrupt everyday carry shabby chic vape cold-pressed, pok pok kickstarter pickled dreamcatcher. Squid occupy activated charcoal, 
+    schlitz leggings pinterest chicharrones kitsch church-key. Direct trade lyft tofu bushwick single-origin coffee, deep v yuccie mlkshk drinking vinegar 
+    forage next level meditation ugh health goth. Lomo bushwick pok pok lo-fi XOXO pug four loko artisan brooklyn irony.`,
 
-    thirdParagraph: `He's the worst.`
+    thirdParagraph: `Pour-over kickstarter kale chips air plant unicorn. Hashtag small batch taxidermy fanny pack, bespoke etsy yuccie vice PBR&B pitchfork 
+    actually dreamcatcher hoodie. Vexillologist echo park knausgaard drinking vinegar brunch locavore. Meggings poutine gastropub drinking vinegar, yr 
+    everyday carry green juice vice literally taiyaki migas umami venmo sustainable sriracha. Distillery pickled pabst ennui. Messenger bag church-key 
+    farm-to-table helvetica, semiotics pug chambray williamsburg poutine beard four loko austin plaid small batch. Affogato truffaut vinyl crucifix pok 
+    pok kale chips kombucha cornhole normcore.`
   },
   {
-    title: `Humanity on Mars!?`,
+    title: `Gibson and postconceptual desemioticism`,
     date: `Apr 14th, 2020`,
-    firstParagraph: ``,
+    firstParagraph: `Farm-to-table shabby chic gluten-free hammock biodiesel bitters. Single-origin coffee meggings offal, twee raclette drinking vinegar
+     health goth leggings gastropub bitters. Af bicycle rights franzen godard knausgaard. Hammock wayfarers pabst tattooed iceland echo park. Bitters 
+     listicle 90's lyft meditation. Tofu occupy typewriter, sustainable raclette swag fingerstache yr chicharrones gochujang pok pok. You probably haven't
+      heard of them celiac gluten-free shoreditch gastropub, microdosing next level palo santo ethical chillwave cred sriracha four loko.`,
 
-    secondParagraph: ``,
+    secondParagraph: `Fanny pack cray hoodie cloud bread leggings. Keffiyeh distillery taxidermy fingerstache microdosing ethical williamsburg pork belly 
+    pok pok retro tacos fam tumeric aesthetic. Venmo polaroid sriracha, squid microdosing post-ironic semiotics farm-to-table tattooed waistcoat fixie 
+    tousled PBR&B. Normcore messenger bag retro raclette unicorn air plant, taiyaki brooklyn helvetica organic woke narwhal pok pok. Enamel pin small batch 
+    seitan forage you probably haven't heard of them, poke affogato 8-bit plaid craft beer fingerstache cred irony. Chartreuse DIY enamel pin meggings 
+    skateboard semiotics helvetica health goth bespoke PBR&B etsy 3 wolf moon forage.`,
 
-    thirdParagraph: ``
+    thirdParagraph: `Meh leggings gochujang live-edge, glossier fashion axe humblebrag pickled. Activated charcoal seitan kickstarter, beard vexillologist 
+    90's adaptogen selfies slow-carb la croix put a bird on it. Listicle lomo single-origin coffee, cold-pressed selvage williamsburg poutine tote bag 
+    jean shorts swag subway tile biodiesel. Lo-fi blue bottle tumblr poke readymade viral deep v taxidermy ugh occupy ramps ennui mustache. Vexillologist 
+    pabst tote bag umami tattooed shaman sriracha before they sold out everyday carry skateboard tousled chartreuse DIY brooklyn freegan.`
   },
   {
-    title: `It's Official: World Hunger is No More!`,
+    title: `Consensuses of collapse`,
     date: `Apr 14th, 2020`,
-    firstParagraph: ``,
+    firstParagraph: `Society is fundamentally a legal fiction,” says Baudrillard; however,
+    according to Brophy , it is not so much society that is
+    fundamentally a legal fiction, but rather the economy, and hence the absurdity,
+    of society. Lyotard uses the term ‘rationalism’ to denote the bridge between
+    sexual identity and society. In a sense, the main theme of the works of Joyce
+    is the paradigm, and subsequent futility, of textual language.`,
 
-    secondParagraph: ``,
+    secondParagraph: `“Truth is elitist,” says Debord; however, according to Humphrey , it is not so much truth that is elitist, but rather the
+    economy of truth. A number of narratives concerning the role of the observer as
+    artist exist. Therefore, Lacan uses the term ‘capitalist deappropriation’ to
+    denote the collapse, and subsequent economy, of subdialectic class.`,
 
-    thirdParagraph: ``
+    thirdParagraph: `In the works of Eco, a predominant concept is the concept of dialectic
+    narrativity. The subject is interpolated into a rationalism that includes
+    consciousness as a paradox. It could be said that in The Name of the
+    Rose, Eco denies Derridaist reading; in The Island of the Day
+    Before, although, he affirms Marxist socialism.`
   }
 ];
 
@@ -166,7 +199,8 @@ function articleCreator(article){
     articleDiv.appendChild(paragraph3);
 
     let expandButton = document.createElement(`span`);
-    expandButton.textContent = "Expand";
+    expandButton.textContent = `\u234c`; //how would I use an HTML character here?
+    expandButton.style.fontSize = "1.6rem";
     expandButton.classList.add(`expandButton`);
     expandButton.addEventListener(`click`, event => {
       articleDiv.classList.toggle(`article-open`);
